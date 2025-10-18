54f2de93b50300c6a3534342d09a973e50dc07ee
ryleu <69326171+ryleu@users.noreply.github.com>
2025-10-18 10:39:40 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author Riley McLain (rjm0037@uah.edu)
+ * @brief Tracking antenna code, controls motor based on GPS information
+ *
+ */
+
+//----------//
+// Includes //
+//----------//
+
+#include <Arduino.h>
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
